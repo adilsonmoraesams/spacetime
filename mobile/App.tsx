3d9edab75d9f69892d83a7886c2ce11268b187c9
Adilson M. (N5) <adilsonmoraes.ams@gmail.com>
2023-07-17 11:35:54 -0400

    Laytout #1

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,31 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts
+} from '@expo-google-fonts/roboto';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import {
+  BaiJamjuree_700Bold
+} from '@expo-google-fonts/bai-jamjuree';
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  });
+
+  if(!hasLoadedFonts){
+    return null;
+  }
+
   return (
-    <View className='bg-gray-950 flex-1 items-center justify-center' >
-      <Text className='text-zinc-50 font-bold text-5xl'>Consegui bbb guri</Text>
+    <View className='bg-gray-900 flex-1 items-center justify-center' >
+      <Text className='text-zinc-50 text-5xl font-title '>Anda</Text>
       <StatusBar style="light" translucent />
     </View>
   );
 } 
+
