1c06e26d16901e316ef281735af41f815c91016e
Adilson M (PC) <adilsonmoraes.ams@gmail.com>
2023-08-23 21:47:02 -0400

    Home page ok

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,31 +1,69 @@
 import {
   Roboto_400Regular,
   Roboto_700Bold,
-  useFonts
-} from '@expo-google-fonts/roboto';
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
-import {
-  BaiJamjuree_700Bold
-} from '@expo-google-fonts/bai-jamjuree';
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import bgBlur from './src/assets/bg-blur.png'
+
+import { styled } from 'nativewind'
+import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
+import Stripes from './src/assets/stripes.svg'
+
+const StyleStrips = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
-    BaiJamjuree_700Bold
-  });
+    BaiJamjuree_700Bold,
+  })
 
-  if(!hasLoadedFonts){
-    return null;
+  if (!hasLoadedFonts) {
+    return null
   }
 
   return (
-    <View className='bg-gray-900 flex-1 items-center justify-center' >
-      <Text className='text-zinc-50 text-5xl font-title '>Anda</Text>
-      <StatusBar style="light" translucent />
-    </View>
-  );
-} 
+    <ImageBackground
+      source={bgBlur}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyleStrips className="absolute left-2" />
 
+      <View className="flex-1 items-center justify-center gap-6">
+        <NlwLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua Capsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-base leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  )
+}
